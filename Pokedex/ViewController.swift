769d595eab3fcaad2c713e2aa1604d56c5b45a73
Gianmarco Cotellessa <gcotellessa@deloitte.it>
2021-11-09 21:02:02 +0100

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Cotellessa, Gianmarco on 09/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
